e0c5c24fa1d76eaacda623219f34e8c095f76542
rfree2monero <rfreemonero@op.pl>
2015-05-26 12:46:59 +0200

    wip skel

---
+++ src/libs/skel/skel.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// @author gloriouscpp
+/// @brief ... copy this file, then edit this description ...
+
+#pragma once
+#ifndef INCLUDE_GLORIOUS_SKEL
+#define INCLUDE_GLORIOUS_SKEL
+
+#include "utils.hpp"
+
+namespace glor {
+namespace skel {
+
+} // namespace 
+} // namespace 
+
+#endif
+
