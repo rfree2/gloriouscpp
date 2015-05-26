fcd5a8e0ef1ffca1251136baf8521f44c89d0f65
rfree2monero <rfreemonero@op.pl>
2015-05-26 12:57:06 +0200

    [ok] cmake, skel, ipc

---
+++ src/libs/ipc/ipc.cpp
@@ -0,0 +1,12 @@
+/// @file
+/// @author gloriouscpp
+/// @brief ... copy this file, then edit this description ...
+
+#include "utils.hpp"
+
+namespace glor {
+namespace ipc {
+
+} // namespace 
+} // namespace 
+
