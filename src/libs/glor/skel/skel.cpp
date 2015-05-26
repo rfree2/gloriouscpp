0a9e565b9156c58fb3f7a2c2f610da3e6aa7f6e2
Robert <rob@mail.l>
2015-05-26 13:33:39 +0200

    includes in ipc.cpp, skel.cpp

--- src/libs/glor/skel/skel.cpp
+++ src/libs/glor/skel/skel.cpp
@@ -2,7 +2,7 @@
 /// @author gloriouscpp
 /// @brief ... copy this file, then edit this description ...
 
-#include "utils.hpp"
+#include <glor/system/utils.hpp>
 
 namespace glor {
 namespace skel {
